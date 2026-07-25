ea6d5331c01de7843f18a4c5996643120c6695ed
Debbal Lotfi <debbal.lotfi.dev@gmail.com>
2026-07-25 20:57:41 +0100

    migrate to esp-idf

---
+++ esp-idf/include/NetworkSetup.h
@@ -0,0 +1,147 @@
+#pragma once
+
+#include <AsyncTCP.h>
+#include <WiFi.h>
+#include <ETH.h>
+#include <NTPClient.h>
+#include "preferences/Network.h"
+#include "preferences/Timezone.h"
+#include "Mqtt.h"
+#include "Led.h"
+#include "Vars.h"
+
+#define NTP_UPDATE_TASK_STACK_SIZE 4096
+#define NTP_UPDATE_INTERVAL_MS 60000
+
+static NTPClient timeClient(networkUDP, "pool.ntp.org", 0, 60000);
+
+static void ntpUpdateTask(void *pvParameters)
+{
+  for (;;)
+  {
+    if (ethernetConnected)
+    {
+      timeClient.update();
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(NTP_UPDATE_INTERVAL_MS));
+  }
+}
+
+// Call once from setup().
+void initNtpUpdateTask()
+{
+  xTaskCreatePinnedToCoreWithCaps(
+      ntpUpdateTask,
+      "ntp_update_worker",
+      NTP_UPDATE_TASK_STACK_SIZE,
+      nullptr,
+      1,
+      nullptr,
+      tskNO_AFFINITY,
+      MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+}
+
+void syncTime()
+{
+  Serial.println("NTP: Syncing time...");
+
+  TimezoneData *timezoneData = loadTimezoneData();
+
+  // Keep the system clock in UTC
+  timeClient.setTimeOffset(0);
+  timeClient.begin();
+
+  while (!timeClient.forceUpdate())
+  {
+    Serial.println("NTP: Waiting for sync...");
+    delay(2000);
+  }
+
+  time_t epoch = timeClient.getEpochTime();
+
+  struct timeval tv = {
+      .tv_sec = epoch,
+      .tv_usec = 0,
+  };
+
+  settimeofday(&tv, nullptr);
+
+  // Configure local timezone
+  setenv("TZ", timezoneData->timezonePosix, 1);
+  tzset();
+  Serial.printf("POSIX TZ: %s\n", timezoneData->timezonePosix);
+
+  ntpSynced = true;
+
+  printCurrentTime();
+}
+
+void WiFiEvent(arduino_event_id_t event)
+{
+  switch (event)
+  {
+  case ARDUINO_EVENT_ETH_START:
+    Serial.println("ETH: Started");
+    ETH.setHostname("esp32-ethernet");
+    break;
+
+  case ARDUINO_EVENT_ETH_CONNECTED:
+    Serial.println("ETH: Link up");
+    break;
+
+  case ARDUINO_EVENT_ETH_GOT_IP:
+    Serial.print("ETH: IP -> ");
+    Serial.println(ETH.localIP());
+    ethernetConnected = true;
+    updateLed();
+    syncTime();
+    if (!mqttStarted)
+    {
+      Serial.println("MQTT: Starting client...");
+      startMqtt();
+      mqttStarted = true;
+      updateLed();
+    }
+    break;
+
+  case ARDUINO_EVENT_ETH_DISCONNECTED:
+    Serial.println("ETH: Link down");
+    ethernetConnected = false;
+    updateLed();
+    break;
+
+  case ARDUINO_EVENT_ETH_STOP:
+    Serial.println("ETH: Stopped");
+    ethernetConnected = false;
+    updateLed();
+    break;
+
+  default:
+    break;
+  }
+}
+
+void setupNetwork()
+{
+  NetworkData *networkData = loadNetworkData();
+
+  Network.onEvent(WiFiEvent);
+
+  ETH.begin(ETH_PHY_W5500, 1, WIZNET_CS_PIN, WIZNET_INT_PIN, WIZNET_RESET_PIN, SPI2_HOST,
+            WIZNET_SCLK_PIN, WIZNET_MISO_PIN, WIZNET_MOSI_PIN);
+
+  if (networkData->ipAssignment == IPAssignment::STATIC)
+  {
+    IPAddress ip(networkData->ip);
+    IPAddress gateway(networkData->gateway);
+    IPAddress subnet(networkData->subnet);
+    IPAddress dns(networkData->dns);
+
+    ETH.config(ip, gateway, subnet, dns);
+  }
+  else
+  {
+    Serial.println("NETWORK-INTERFACE: Using DHCP");
+  }
+}
---
+++ esp-idf/include/controllers/MqttController.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include <Arduino.h>
+#include <AsyncTCP.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
+#include <esp_now.h>
+#include <etl/string.h>
+#include "esp_http_client.h"
+#include "esp_crt_bundle.h"
+#include "Properties.h"
+#include "Vars.h"
+#include "Utils.h"
+#include "preferences/Mqtt.h"
+
+class MqttController
+{
+public:
+  static void getConfig(AsyncWebServerRequest *request)
+  {
+    MqttData *mqttData = loadMqttData();
+    AsyncJsonResponse *response = new AsyncJsonResponse();
+    JsonObject root = response->getRoot().to<JsonObject>();
+    auto config = root["config"].to<ArduinoJson::JsonObject>();
+
+    root["isSet"] = mqttData->isSet;
+    if (mqttData->isSet)
+    {
+      config["url"] = mqttData->url;
+      config["clientId"] = mqttData->clientId;
+      config["useAuth"] = mqttData->useAuth;
+      if (mqttData->useAuth)
+      {
+        config["username"] = mqttData->username;
+        config["password"] = mqttData->password;
+      }
+      config["useSleepyPeerDiscovery"] = mqttData->useSleepyPeerDiscovery;
+      if (mqttData->useSleepyPeerDiscovery)
+      {
+        config["discoveryRequestTopic"] = mqttData->discoveryRequestTopic;
+        config["discoveryResponseTopic"] = mqttData->discoveryResponseTopic;
+      }
+    }
+
+    response->setLength();
+    request->send(response);
+  }
+
+  static void setConfig(AsyncWebServerRequest *request, ArduinoJson::JsonVariant &json)
+  {
+    if (
+        !json["clientId"].is<const char *>() ||
+        !json["url"].is<const char *>() ||
+        !json["useAuth"].is<bool>() ||
+        !json["useSleepyPeerDiscovery"].is<bool>())
+    {
+      request->send(400, "application/json", "{\"error\":\"INVALID_REQUEST\"}");
+      return;
+    }
+    const char *clientId = json["clientId"].as<const char *>();
+    const char *url = json["url"].as<const char *>();
+    if (!isValidMqttUrl(url))
+    {
+      request->send(400, "application/json", "{\"error\":\"INVALID_REQUEST\"}");
+      return;
+    }
+    bool useAuth = json["useAuth"].as<bool>();
+    if (useAuth && (!json["username"].is<const char *>() || !json["password"].is<const char *>()))
+    {
+      request->send(400, "application/json", "{\"error\":\"INVALID_REQUEST\"}");
+      return;
+    }
+    bool useSleepyPeerDiscovery = json["useSleepyPeerDiscovery"].as<bool>();
+    if (useSleepyPeerDiscovery && (!json["discoveryRequestTopic"].is<const char *>() || !json["discoveryResponseTopic"].is<const char *>()))
+    {
+      request->send(400, "application/json", "{\"error\":\"INVALID_REQUEST\"}");
+      return;
+    }
+
+    MqttData *mqttData = loadMqttData();
+
+    mqttData->isSet = true;
+    strlcpy(mqttData->clientId, clientId, MQTT_CLIENT_ID_SIZE);
+    strlcpy(mqttData->url, url, MQTT_URL_SIZE);
+    if (useAuth)
+    {
+      const char *username = json["username"].as<const char *>();
+      const char *password = json["password"].as<const char *>();
+      mqttData->useAuth = true;
+      strlcpy(mqttData->username, username, MQTT_USERNAME_SIZE);
+      strlcpy(mqttData->password, password, MQTT_PASSWORD_SIZE);
+    }
+    else
+    {
+      mqttData->useAuth = false;
+    }
+
+    if (useSleepyPeerDiscovery)
+    {
+      const char *discoveryRequestTopic = json["discoveryRequestTopic"].as<const char *>();
+      const char *discoveryResponseTopic = json["discoveryResponseTopic"].as<const char *>();
+      mqttData->useSleepyPeerDiscovery = true;
+      strlcpy(mqttData->discoveryRequestTopic, discoveryRequestTopic, TOPIC_SIZE);
+      strlcpy(mqttData->discoveryResponseTopic, discoveryResponseTopic, TOPIC_SIZE);
+    }
+    else
+    {
+      mqttData->useSleepyPeerDiscovery = false;
+    }
+
+    saveMqttData(mqttData);
+    request->send(200);
+
+    delayedRestart();
+  }
+};
